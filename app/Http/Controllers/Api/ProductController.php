30bacd7f38328d2611557989fda0937d4ca0d19b
amedeopro <amedeopro@me.com>
2019-03-03 19:28:01 +0100

    show

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Product;
+class ProductController extends Controller
+{
+    public function index(){
+
+      $products = Product::all();
+
+      return response()->json($products);
+
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -12,7 +12,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::get('/', function(){
+  return 'Ciao siamo nell\'api';
 });
+
+Route::get('/products','Api\ProductController@index');
